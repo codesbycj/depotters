2a708e14c82e2dfb4f96bc770c9d427d8139a02d
Developergatsby <gabokekeemmanuel@gmail.com>
2025-08-03 06:15:36 +0100

    No testimonial

--- index.php
+++ index.php
@@ -353,7 +353,6 @@
 
 
     <!-- SLICK FOR THE TESTIMONIAL -->
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
 
     <!-- BOOTSTRAP JS -->
