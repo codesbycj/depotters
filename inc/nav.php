c51e3a069c660c5692626de38c06b67e4f245b11
Developergatsby <gabokekeemmanuel@gmail.com>
2025-08-29 10:04:44 +0100

    Finalize depotters

--- inc/nav.php
+++ inc/nav.php
@@ -1,10 +1,10 @@
     <nav class="navbar navbar-expand-lg position-fixed py-lg-3 px-lg-5 px-3 py-2 z-3">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="assets/logo.png" alt="" style="width: 150px;"></a>
+            <a class="navbar-brand" href="#"><img src="assets/logo.png" alt="" style="width: 110px;"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon" style="width: 25px;"></span>
             </button>
             <div class="collapse navbar-collapse mt-5 mt-lg-0" id="navbarSupportedContent">
                 <ul class="navbar-nav mx-auto mb-lg-0 gap-5 fw-bold">
